cdaacd5cca10a70284a155757e8b8304af3ab93d
Simon Wiles <simonjwiles@gmail.com>
2024-05-29 21:24:00 -0700

    Solid full-page layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<script>
+	import '../styles/reset.css';
+	import '../styles/base.css';
+</script>
+
+<header>
+	<h1>Jewish Cookbooks</h1>
+</header>
+
+<main>
+	<slot />
+</main>
+
+<footer>GitHub</footer>
+
+<style>
+	header {
+	}
+
+	main {
+		overflow: hidden;
+		flex: 1 1 auto;
+		display: flex;
+		flex-direction: column;
+	}
+
+	footer {
+		align-self: flex-end;
+	}
+</style>
