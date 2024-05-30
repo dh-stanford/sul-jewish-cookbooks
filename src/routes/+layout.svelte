f77715defee918a0ef8509ef9c2be7c8fd0cf6a7
Simon Wiles <simonjwiles@gmail.com>
2024-05-29 21:50:17 -0700

    Add title and description

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,11 @@
 	import '../styles/base.css';
 </script>
 
+<svelte:head>
+	<title>Jewish Cookbooks</title>
+	<meta name="description" content="Recipes from Stanford LIbraries' Jewish Cookbooks Collection" />
+</svelte:head>
+
 <header>
 	<h1>Jewish Cookbooks</h1>
 </header>
