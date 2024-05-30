15e15805cdbe708ca1e465bcec61348c85b5f167
Simon Wiles <simonjwiles@gmail.com>
2024-05-30 12:25:05 -0700

    Simple `<SearchInput />` component

---
+++ src/components/SearchInput.svelte
@@ -0,0 +1,35 @@
+<script>
+	let { ...props } = $props();
+
+	const searchSvg = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor">
+      <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+      <path d="M21 21l-6 -6" />
+    </svg>  
+  `;
+</script>
+
+<label style="--svg:url(data:image/svg+xml;utf8,{encodeURIComponent(searchSvg)})">
+	<input
+		aria-label="Search"
+		aria-multiline="false"
+		placeholder="Search..."
+		spellcheck="false"
+		tabindex="0"
+		{...props}
+	/>
+</label>
+
+<style>
+	input {
+		background-image: var(--svg);
+		background-position: left 0.5rem center;
+		background-repeat: no-repeat;
+		background-size: 1.1rem;
+		border-radius: var(--border-radius, 5px);
+		border: 1px solid var(--primary-color);
+		color: var(--primary-color);
+		outline-color: var(--primary-color);
+		padding: 0.5rem 0.5rem 0.5rem 2rem;
+	}
+</style>
